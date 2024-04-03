6054a0aa56cd0e452767e4d8c1843be880431933
Lucy Holmes <lucyholmes08@hotmail.com>
2024-04-03 15:12:58 +0100

    chalk npm

---
+++ script.js
@@ -0,0 +1,22 @@
+import chalk from "chalk";
+
+const log = console.log;
+
+log(chalk.white.bgRed.bold("Hello World!"));
+log(chalk.white.bgYellow.bold("Hello World!"));
+log(chalk.white.bgGreen.bold("Hello World!"));
+log(chalk.white.bgCyan.bold("Hello World!"));
+log(chalk.white.bgBlue.bold("Hello World!"));
+log(chalk.white.bgMagenta.bold("Hello World!"));
+log(chalk.white.bgRed.bold("Hello World!"));
+log(chalk.white.bgYellow.bold("Hello World!"));
+log(chalk.white.bgGreen.bold("Hello World!"));
+log(chalk.white.bgCyan.bold("Hello World!"));
+log(chalk.white.bgBlue.bold("Hello World!"));
+log(chalk.white.bgMagenta.bold("Hello World!"));
+log(chalk.white.bgRed.bold("Hello World!"));
+log(chalk.white.bgYellow.bold("Hello World!"));
+log(chalk.white.bgGreen.bold("Hello World!"));
+log(chalk.white.bgCyan.bold("Hello World!"));
+log(chalk.white.bgBlue.bold("Hello World!"));
+log(chalk.white.bgMagenta.bold("Hello World!"));
